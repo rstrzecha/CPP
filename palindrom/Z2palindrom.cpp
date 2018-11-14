5d67776eb0db5d25b08e00d004d096113308bfd0
Robert <44380843+rstrzecha@users.noreply.github.com>
2018-11-14 22:14:13 +0100

    Create Z2palindrom.cpp

---
+++ palindrom/Z2palindrom.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    string zdanie;
+    char *wsk_name;
+    bool wynik = 1;
+    int i, j;
+
+    cout << "Podaj zdanie: ";
+    getline(cin, zdanie);
+    int k = zdanie.length();
+
+    wsk_name = new char [k];
+    for(i = 0, j = 0; j < k+1; i++, j++)
+    {
+        if(zdanie[j] != 32) wsk_name[i] = tolower(zdanie[j]);
+        else i--;
+    }
+
+    cout << "wsk_name: " << wsk_name << endl;
+    k = strlen(wsk_name);
+
+    for(i = 0; i < k/2; i++)
+    {
+        if (*(wsk_name+i) != *(wsk_name+k-1-i))
+        {
+            wynik = 0;
+            break;
+        }
+    }
+    if(wynik) cout << "Polindrom";
+    else
+    {
+        cout << "Nie polindrom" << endl;
+        for (i = 0; i < k/2; i++)
+            *(wsk_name+k-1-i) = *(wsk_name+i);
+        cout << "Proponowana zmiana - " << wsk_name;
+    }
+
+    delete wsk_name;
+    return 0;
+}
